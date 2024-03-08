6e018564267d5d1d1dff3e94734c03ff7fca4913
BhaweshEiysys <bhawesh@eiysys.in>
2024-03-08 16:49:45 +0530

    fuzuki-inu

---
+++ src/component/Home.js
@@ -0,0 +1,80 @@
+import BrandLogo from "../assets/images/logo.png"
+
+import FuzikuMedia from "../assets/images/media1.png"
+
+import BtnImg from "../assets/images/buy-now.png"
+
+import IcFacebook from "../assets/images/icons/ic-facebook.svg"
+import IcTwitter from "../assets/images/icons/ic-twitter.svg"
+import IcLinkedin from "../assets/images/icons/ic-linkedin.svg"
+import IcTelegram from "../assets/images/icons/ic-telegram.svg"
+
+function Home() {
+    return (
+        <div className="home-wrapper">
+            <header>
+                <div className="container">
+                    <a href="javascript:;" className="brand-logo">
+                        <img src={BrandLogo} alt="logo" />
+                    </a>
+                </div>
+            </header>
+            <section className="fuziku-info">
+                <div className="container">
+                    <div className="fuziku-info-content">
+                        <div className="row">
+                            <div className="col-xl-6 ">
+                                <div className="fuziku-info-media">
+                                    <div className="media-img">
+                                        <img src={FuzikuMedia} alt="fuziku media" />
+                                    </div>
+                                    <a href="javacript:;" className="buy-now-btn">
+                                        <img src={BtnImg} alt="button" />
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="col-xl-6">
+                                <div className="fuziku-info-text">
+                                    <p>It's a guarantee that Fizuki Inu will outshine other regular coins. What's your smartest choice? Do you want to stand out in the world of coins? Take a look at our Fuzuki Inu, - The Shiba Kanji!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-xl-7">
+                            <div className="desclaimer-text">
+                                <p className="mb-3"><span>Disclaimer:</span> The presence of fox images with Fuzuki Inu represents the creativity and customization that motivate our blockchain initiative.</p>
+                                <p>There are no actual, real, or mythical foxes involved. Fuzuki Inu is our way of exploring digital innovation and building a sense of unity.</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-5 d-flex justify-content-end">
+                            <div className="footer-social">
+                                <ul>
+                                    <li>
+                                        <a href="javascript:;"><img src={IcFacebook} alt="facebook icon" /></a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;"><img src={IcTwitter} alt="facebook icon" /></a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;"><img src={IcLinkedin} alt="facebook icon" /></a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;"><img src={IcTelegram} alt="facebook icon" /></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+
+export default Home;
