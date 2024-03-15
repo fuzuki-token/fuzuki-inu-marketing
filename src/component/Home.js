ad2d55345b6ad18647b2584f9917b67519ac9633
manishjoshi <manishjoshi@eiysys.in>
2024-03-15 18:10:58 +0530

    add road map section to design

--- src/component/Home.js
+++ src/component/Home.js
@@ -11,6 +11,11 @@ import IcInstagram from "../assets/images/icons/ic-instagram.svg"
 import IcYoutube from "../assets/images/icons/ic-youtube.svg"
 import IcReddit from "../assets/images/icons/ic-reddit.svg"
 
+
+import StageImg1 from "../assets/images/stage-1.png"
+import StageImg2 from "../assets/images/stage-2.png"
+import StageImg3 from "../assets/images/stage-3.png"
+
 function Home() {
     return (
         <div className="home-wrapper">
@@ -37,12 +42,82 @@ function Home() {
                             </div>
                             <div className="col-md-6">
                                 <div className="fuziku-info-text">
-                                    <p>Invest and develop compassion with Fuzuki Inu to ensure a brighter tomorrow. Hold with conviction, sprinting towards success with <br/> <span className="brand">Fuzuki Inu: The shiba kanji</span>
-                                    <a href="https://fuzuki-s3.s3.amazonaws.com/fuzuki-doc/Fuzuki+Inu+(1).pdf" className="whitepaper-link">Whitepaper</a></p>
+                                    <p>Invest and develop compassion with Fuzuki Inu to ensure a brighter tomorrow. Hold with conviction, sprinting towards success with <br /> <span className="brand">Fuzuki Inu: The shiba kanji</span>
+                                        <a target="_blank" href="https://fuzuki-s3.s3.amazonaws.com/fuzuki-doc/Fuzuki+Inu+(1).pdf" className="whitepaper-link">Whitepaper</a></p>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </section>
+
+            <section className="road-map">
+                <div className="container">
+                    <div className="section-title">
+                        <h4>Road Map</h4>
+                    </div>
+
+                    <ul className="road-map-stages-list">
+                        <li className="row">
+                            <div className="col-lg-4">
+                                <div className="stage-media mb-4">
+                                    <img src={StageImg1} alt="StageImg1" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8">
+                                <div className="road-map-stage">
+                                    <h4>launch</h4>
+                                    <ul>
+                                        <li>Public sale begins first opportunity to buy Fuzuki Inu coins. </li>
+                                        <li>Creating the project idea, logo, and tokenomics. </li>
+                                        <li>Establishing an online network through social media platforms.</li>
+                                        <li>Launching marketing campaigns to attract possible investors. </li>
+                                        <li>Uniswap Launch</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="row flex-lg-row-reverse">
+                            <div className="col-lg-4 text-lg-end">
+                                <div className="stage-media mb-4">
+                                    <img src={StageImg2} alt="StageImg2" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8">
+                                <div className="road-map-stage">
+                                    <h4>CEX Listing</h4>
+                                    <ul>
+                                        <li>CEX listing: Fuzuki Inu coins will now be offered on centralized exchanges.</li>
+                                        <li>Beginning with the second stage of the marketing campaign round.</li>
+                                        <li>Partnering with other meme-based projects.</li>
+                                        <li>Offer new income sources for token holders.</li>
+                                        <li>Listing for CMC and CG. </li>
+                                        <li>Listing on CEX Platforms </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="row">
+                            <div className="col-lg-4">
+                                <div className="stage-media mb-4">
+                                    <img src={StageImg3} alt="StageImg3" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8">
+                                <div className="road-map-stage">
+                                    <h4>DEX Launch & New Token Launch (Skulk)</h4>
+                                    <ul>
+                                        <li>Introduction to Decentralized Exchanges.</li>
+                                        <li>New token (Skulk) launch: Expands the Fuzuki Inu ecosystem.</li>
+                                        <li>Setting up marketing campaigns for stage 3.</li>
+                                        <li>Established multiple collaborations to extend the initiative.</li>
+                                        <li>Marketing to reach new marketplaces and groups for the project.</li>
+                                        <li>Creating new revenue streams and partnerships to increase project income.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </section>
             <footer>
@@ -57,22 +132,22 @@ function Home() {
                             <div className="footer-social">
                                 <ul>
                                     <li>
-                                        <a href="https://m.facebook.com/fuzuki.inu"><img src={IcFacebook} alt="facebook icon" /></a>
+                                        <a target="_blank" href="https://m.facebook.com/fuzuki.inu"><img src={IcFacebook} alt="facebook icon" /></a>
                                     </li>
                                     <li>
-                                        <a href="https://twitter.com/Fuzuki_Inu"><img src={IcTwitter} alt="twitter icon" /></a>
+                                        <a target="_blank" href="https://twitter.com/Fuzuki_Inu"><img src={IcTwitter} alt="twitter icon" /></a>
                                     </li>
                                     <li>
-                                        <a href="https://t.me/fuzukitoken"><img src={IcTelegram} alt="Telegram icon" /></a>
+                                        <a target="_blank" href="https://t.me/fuzukitoken"><img src={IcTelegram} alt="Telegram icon" /></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.instagram.com/fuzukiinu5/"><img src={IcInstagram} alt="instagram icon" /></a>
+                                        <a target="_blank" href="https://www.instagram.com/fuzukiinu5/"><img src={IcInstagram} alt="instagram icon" /></a>
                                     </li>
                                     <li>
-                                        <a href=" https://www.youtube.com/@FuzukiArt"><img src={IcYoutube} alt="Youtube icon" /></a>
+                                        <a target="_blank" href=" https://www.youtube.com/@FuzukiArt"><img src={IcYoutube} alt="Youtube icon" /></a>
                                     </li>
                                     <li>
-                                        <a href=" https://www.reddit.com/user/fuzuki_Inu/"><img src={IcReddit} alt="Reddit icon" /></a>
+                                        <a target="_blank" href=" https://www.reddit.com/user/fuzuki_Inu/"><img src={IcReddit} alt="Reddit icon" /></a>
                                     </li>
                                 </ul>
                             </div>
