e5cd3d163f85000fd130ccfdca503de98dea3802
manishjoshi <manishjoshi@eiysys.in>
2024-03-15 18:25:38 +0530

    fix bugs

--- src/component/Home.js
+++ src/component/Home.js
@@ -43,7 +43,7 @@ function Home() {
                             <div className="col-md-6">
                                 <div className="fuziku-info-text">
                                     <p>Invest and develop compassion with Fuzuki Inu to ensure a brighter tomorrow. Hold with conviction, sprinting towards success with <br /> <span className="brand">Fuzuki Inu: The shiba kanji</span>
-                                        <a target="_blank" href="https://fuzuki-s3.s3.amazonaws.com/fuzuki-doc/Fuzuki+Inu+(1).pdf" className="whitepaper-link">Whitepaper</a></p>
+                                        <a target="_blank" href="https://fuzuki-s3.s3.amazonaws.com/fuzuki-doc/Fuzuki+Inu.pdf" className="whitepaper-link">Whitepaper</a></p>
                                 </div>
                             </div>
                         </div>
@@ -66,7 +66,7 @@ function Home() {
                             </div>
                             <div className="col-lg-8">
                                 <div className="road-map-stage">
-                                    <h4>launch</h4>
+                                    <h4>Launch</h4>
                                     <ul>
                                         <li>Public sale begins first opportunity to buy Fuzuki Inu coins. </li>
                                         <li>Creating the project idea, logo, and tokenomics. </li>
@@ -140,9 +140,9 @@ function Home() {
                                     <li>
                                         <a target="_blank" href="https://t.me/fuzukitoken"><img src={IcTelegram} alt="Telegram icon" /></a>
                                     </li>
-                                    <li>
+                                    {/* <li>
                                         <a target="_blank" href="https://www.instagram.com/fuzukiinu5/"><img src={IcInstagram} alt="instagram icon" /></a>
-                                    </li>
+                                    </li> */}
                                     <li>
                                         <a target="_blank" href=" https://www.youtube.com/@FuzukiArt"><img src={IcYoutube} alt="Youtube icon" /></a>
                                     </li>
